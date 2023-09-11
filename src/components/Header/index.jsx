3b06e2863d0defb8c96c1646f46829c494ae226d
Lê Quốc Huy <97956803+QuocHuy03@users.noreply.github.com>
2023-09-11 12:29:17 +0700

    update

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React from "react";
+import HeaderTop from "../HeaderTop";
+import HeaderBottom from "../HeaderBottom";
 
 export default function Header() {
   return (
-    <div>Header</div>
-  )
+    <div className="global-header-container bg-blue">
+      <HeaderTop />
+      <HeaderBottom />
+    </div>
+  );
 }
