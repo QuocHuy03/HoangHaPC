805875846011a3f5c973b4213502808bea8fa04d
hunglhh <lehuuhung0903@gmail.com>
2023-09-11 20:40:54 +0700

    Update index.jsx

--- src/components/HeaderTop/index.jsx
+++ src/components/HeaderTop/index.jsx
@@ -1,5 +1,6 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+
+import { Link } from 'react-router-dom';
 
 export default function HeaderTop() {
   return (
