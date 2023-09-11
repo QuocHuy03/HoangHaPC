5de3ad63724b61e3cbfea43583419f2980127be4
Lê Quốc Huy <97956803+QuocHuy03@users.noreply.github.com>
2023-09-11 12:45:32 +0700

    update 12h45-11/9/2023
    update slider swiper

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
-
-export default function Carousel() {
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+export default function Carousel({ images }) {
   return (
-    <div>Carousel</div>
-  )
+    <Swiper className="mySwiper">
+      {images?.map((item) => {
+        <SwiperSlide>
+          <img src={item.image} alt={item.name} />
+        </SwiperSlide>;
+      })}
+    </Swiper>
+  );
 }
