5e2627dafc2b64076166a8b563ee9d7440c40ffb
Đinh Tấn Trình <135788544+tantrinh29@users.noreply.github.com>
2023-09-12 15:16:31 +0700

    update blog page 15h10 21/9/2023

--- src/pages/BlogDetailPage/index.jsx
+++ src/pages/BlogDetailPage/index.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import Layout from '../../components/Layout'
 
 export default function BlogDetailPage() {
   return (
-    <div>BlogDetailPage</div>
+    <Layout>BlogDetailPage</Layout>
   )
 }
