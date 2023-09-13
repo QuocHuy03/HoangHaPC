a35c358163cc03b91996a087ac4bddb4e68ba589
hunglhh <lehuuhung0903@gmail.com>
2023-09-13 16:08:58 +0700

    update blog page 4:08 13/9/2023 Hưng

--- src/pages/FilterPage/index.jsx
+++ src/pages/FilterPage/index.jsx
@@ -1,5 +1,6 @@
-import React from "react";
-import Layout from "../../components/Layout";
+import React from 'react';
+
+import Layout from '../../components/Layout';
 
 export default function FilterPage() {
   return (
