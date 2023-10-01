ab8cf11157963081660854a03a21bfaca31dfa94
Lê Quốc Huy <97956803+QuocHuy03@users.noreply.github.com>
2023-10-01 13:47:07 +0700

    update order

--- src/pages/CartPage/index.jsx
+++ src/pages/CartPage/index.jsx
@@ -12,10 +12,10 @@ import {
 import { formatPrice } from "../../utils/fomatPrice";
 import { message } from "antd";
 import { AppContext } from "../../contexts/AppContextProvider";
+import { v4 as uuidv4 } from "uuid";
 
 export default function CartPage() {
   const dispatch = useDispatch();
-
   const { carts, user } = useContext(AppContext);
   const totalAmountAll = carts?.reduce(
     (total, item) => total + item?.product.price_has_dropped * item.quantity,
@@ -274,7 +274,7 @@ export default function CartPage() {
                     </textarea>
                   </div>
                   <Link
-                    to={URL_CONSTANTS.CHECKOUT}
+                    to={`/checkout/${uuidv4()}`}
                     className="btn-submit-cart js-send-cart"
                   >
                     <b>Đặt hàng</b>
