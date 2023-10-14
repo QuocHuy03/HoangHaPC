455cde7f0c02f460e1efdffe0a0282fe1175c2b2
Lê Quốc Huy <97956803+QuocHuy03@users.noreply.github.com>
2023-10-14 11:55:02 +0700

    Create useDebounce.js

---
+++ src/hooks/useDebounce.js
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react";
+
+export const useDebounce = (value, delay) => {
+  const [valueDebounce, setValueDebounce] = useState("");
+  useEffect(() => {
+    const handle = setTimeout(() => {
+      setValueDebounce(value);
+    }, [delay]);
+    return () => {
+      clearTimeout(handle);
+    };
+  }, [value]);
+  return valueDebounce;
+};
